adcf8e176b97d21c9c6000bb04f9a7d590028172
sikapal <sikapalucien.lucien@gmail.com>
2022-12-26 08:25:49 +0100

    Initial commit

---
+++ app/src/main/java/cm/skpcorp/googlelogin/HomeActivity.java
@@ -0,0 +1,69 @@
+package cm.skpcorp.googlelogin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class HomeActivity extends AppCompatActivity {
+
+    TextView name,mail;
+    Button logout;
+
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        name=findViewById(R.id.name);
+        mail=findViewById(R.id.mail);
+        logout=findViewById(R.id.Logout);
+
+
+        gso =new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        gsc = GoogleSignIn.getClient(this,gso);
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if(account!=null){
+            String Name = account.getDisplayName();
+            String Mail =account.getEmail();
+
+            name.setText(Name);
+            mail.setText(Mail);
+        }
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignOut();
+            }
+        });
+
+    }
+    private void SignOut(){
+
+        gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                finish();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+    }
+}
